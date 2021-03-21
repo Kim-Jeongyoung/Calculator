c8675a692d8a107d0cc51ffb2594e70953b1806f
Kim-Jeongyoung <jeongyoung2@gmail.com>
2021-03-21 07:42:54 -0700

    Calculator & BMI Routing Challenge by using javaScript

---
+++ calculator.js
@@ -0,0 +1,37 @@
+
+const express = require("express");
+const bodyParser = require("body-parser")
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post("/", function(req, res) {
+  var n1 = Number(req.body.n1);
+  var n2 = Numebr(req.body.n2);
+  var result = n1 + n2;
+
+  res.send("The result of the BMI Calculation is" + result);
+})
+
+app.get("/bmiCalculator", function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function(req, res){
+
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi = weight / (height*height);
+
+  res.send("Your BMI is " + bmi);
+});
+
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000.");
+});
